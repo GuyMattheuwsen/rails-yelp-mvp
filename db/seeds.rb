8d718afae1f04588902a6b96b8d7fe311e769e70
GuyMattheuwsen <guy.mattheuwsen@gmail.com>
2022-02-16 16:08:26 +0100

    a lot of green, Guy, well done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+my_restaurant = Restaurant.new(name: "L'Assiette Champaqnoise", category: "french", address: "Reims")
+pepe = Restaurant.new(name: "Pepe Nero", category: "italian", address: "Leuven")
+arenberg = Restaurant.new(name: "Arenberg", category: "belgian", address: "Egenhoven")
+ming = Restaurant.new(name: "Ming Dinasty", category: "chinese", address: "Leuven")
+sushi = Restaurant.new(name: "Sushi", category: "japanese", address: "Brussel")
+my_restaurant.save
+pepe.save
+arenberg.save
+ming.save
+sushi.save
+
